fd4f1a9bf0ee8f2bbc0692788c0fbdec9f51c368
rob-vancleave <rob.vancleave@cyberark.com>
2025-10-27 09:40:04 -0400

    updated radix ui

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,13 +5,23 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  css: {
+    devSourcemap: false,
+  },
   build: {
     outDir: 'build',
     sourcemap: false,
+    cssMinify: true,
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
+        },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'assets/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
         },
       },
     },
